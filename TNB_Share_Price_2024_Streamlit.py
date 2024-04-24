320f2244a5bb71252e8ad001aeb62e2001c5e464
mirainsight <108640953+mirainsight@users.noreply.github.com>
2024-04-24 23:50:33 +0800

    Update TNB_Share_Price_2024_Streamlit.py

--- TNB_Share_Price_2024_Streamlit.py
+++ TNB_Share_Price_2024_Streamlit.py
@@ -26,6 +26,7 @@ import pandas as pd
 import random 
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
