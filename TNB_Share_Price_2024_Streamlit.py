ee55ba379953b1a67365d50f0a20167cb005a772
mirainsight <108640953+mirainsight@users.noreply.github.com>
2024-04-24 23:50:03 +0800

    Update TNB_Share_Price_2024_Streamlit.py

--- TNB_Share_Price_2024_Streamlit.py
+++ TNB_Share_Price_2024_Streamlit.py
@@ -26,7 +26,10 @@ import pandas as pd
 import random 
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 
  
 gifs = ["giphy.gif", "ysb.gif", "smol-illegally-smol-cat.gif", 
