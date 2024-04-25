54992cc79fc73df186abb6371e665c92ab374a6b
mirainsight <108640953+mirainsight@users.noreply.github.com>
2024-04-25 21:25:28 +0800

    Update TNB_Share_Price_2024_Streamlit.py

--- TNB_Share_Price_2024_Streamlit.py
+++ TNB_Share_Price_2024_Streamlit.py
@@ -45,7 +45,7 @@ import streamlit as st
 
 text_to_copy = st.text_input("Hello, World!")
 
-hosted_html_file = "https://mira-tnb-share-price-2024.streamlit.app/files/copy.html"
+hosted_html_file = "https://everydayswag.org/files/copy.html"
 iframe_url = f"{hosted_html_file}?copy={text_to_copy}"
 
 st.markdown(f'<iframe style="overflow: hidden;" src="{iframe_url}"></iframe>', unsafe_allow_html=True)
