68f6aeb22af82752a13ce7cd39766532bc426e35
mirainsight <108640953+mirainsight@users.noreply.github.com>
2024-07-10 23:07:39 +0800

    Update TNB_Share_Price_2024_Streamlit.py

--- TNB_Share_Price_2024_Streamlit.py
+++ TNB_Share_Price_2024_Streamlit.py
@@ -132,9 +132,9 @@ if st.button('Calculate share price'):
         try:
             current_volume = int(float(TNB_volume.replace(',', ''))/(10**6))
         except ValueError:
-            current_volume = TNB_volume/(10**6)
-            st.write(f"Hi my name is {current_volume}")
-            st.write(type(current_volume))
+            #current_volume = TNB_volume/(10**6)
+            st.write(f"Hi my name is {TNB_volume}")
+            st.write(type(TNB_volume))
             
 
         st.write("Getting KLCI index... it's only been %s seconds..." % round(t.time() - start_time1, 0))
